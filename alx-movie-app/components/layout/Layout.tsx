78505b2d923b95d760d5d9be560e5b58dff529a3
aaronkefyalew <aaronkefyalew@gmail.com>
2025-12-02 17:20:32 +0300

    setup

---
+++ alx-movie-app/components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
+import React from "react";
+
+export const Layout: React.FC<ComponentProps> = ({ children }) => {
+    return (
+        <>
+       <Header />
+       <main className="min-h-screen">{children}</main>
+        <Footer />
+       </>
+    );
+}
